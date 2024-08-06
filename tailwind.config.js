1dcc07e43ec1c2256e10564778a38c943346a67c
Afkan <afkanozdemir@gmail.com>
2024-08-07 01:18:08 +0300

    chore: Add NativeWind plugin to Babel configuration and update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+//  app\(stack)\_layout.tsx
+
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/**/*.{js,jsx,ts,tsx}",
+    "app/**/*.{js,jsx,ts,tsx}",
+    "app/**/**/*.{js,jsx,ts,tsx}",
+    "app/**/**/**/*.{js,jsx,ts,tsx}",
+    "app/**/**/**/**/*.{js,jsx,ts,tsx}",
+
+  ],
+  theme: {
+    extend: {
+      flex: {
+        2: "2 2 0%",
+        3: "3 3 0%",
+        4: "4 4 0%",
+        5: "5 5 0%",
+        6: "6 6 0%",
+        7: "7 7 0%",
+        8: "8 8 0%",
+        9: "9 9 0%",
+        10: "10 10 0%",
+        11: "11 11 0%",
+        12: "12 12 0%",
+      },
+    },
+  },
+  plugins: [],
+}
+
