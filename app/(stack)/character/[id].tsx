8aca14478a0e24672d3400e073c2c1c4893cc6bf
seyfettinTulum <tulumseyfettin1@gmail.com>
2024-08-08 04:23:29 +0300

    feat: Add lazy loading for images and update dependencies

--- app/(stack)/character/[id].tsx
+++ app/(stack)/character/[id].tsx
@@ -7,17 +7,18 @@ import {
   ScrollView,
   FlatList,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 import { Image } from "expo-image";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import React, { useEffect, useState } from "react";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import React, { useEffect, useRef, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { LinearGradient } from "expo-linear-gradient";
 const API_KEY = process.env.EXPO_PUBLIC_OPENAI_API_KEY;
 // const API_KEY = "sk-1c";
 
 const App = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   const { id } = useLocalSearchParams();
   const [chatMeet, setChatMeet] = useState<string>("");
   const [chat, setChat] = useState<{ role: string; content: string }[]>([]);
@@ -26,6 +27,7 @@ const App = () => {
 
   const _init = async () => {
     try {
+      setLoading(true);
       const response = await fetch(
         "https://api.openai.com/v1/chat/completions",
         {
@@ -54,6 +56,7 @@ const App = () => {
           ...prevChat,
           { role: message.message.role, content: message.message.content },
         ]);
+        setLoading(false);
       }
     } catch (e) {
       console.error(e);
@@ -67,9 +70,14 @@ const App = () => {
   const sendMessage = async (newMessage: string) => {
     try {
       if (newMessage) {
+        setLoading(true);
         setChat((prevChat) => [
           ...prevChat,
           { role: "user", content: newMessage },
+        ]);
+        setChat((prevChat) => [
+          ...prevChat,
+          { role: "asistant", content: "Loading..." },
         ]);
         console.log(...chat);
         const response = await fetch(
@@ -96,10 +104,17 @@ const App = () => {
         const data = await response.json();
         const message = data.choices[0];
         if (message) {
-          setChat((prevChat) => [
-            ...prevChat,
-            { role: message.message.role, content: message.message.content },
-          ]);
+          setTimeout(() => {
+            setChat((prevChat) => {
+              prevChat.pop();
+              return prevChat;
+            });
+            setChat((prevChat) => [
+              ...prevChat,
+              { role: message.message.role, content: message.message.content },
+            ]);
+            setLoading(false);
+          }, 2000);
         }
       }
     } catch (e) {
@@ -111,56 +126,95 @@ const App = () => {
     <SafeAreaView className="flex-1 bg-background">
       <View className="w-full flex-1 flex-row border-b py-2 border-[#1c1c2c] mb-3">
         <View className="flex-1">
-          <Image source={require("../../../assets/ChatGPT-Logo.png")} className="w-full h-full absolute z-0" contentFit="cover" transition={500} />
+          <Image
+            source={require("../../../assets/ChatGPT-Logo.png")}
+            className="w-full h-full absolute z-0"
+            contentFit="cover"
+            transition={500}
+          />
         </View>
         <View className="mx-4 flex-5 flex-row items-center justify-between">
           <View>
             <Text className="font-psemibold text-white">{id}</Text>
           </View>
-          <TouchableOpacity className="p-2 bg-primary rounded-full items-center justify-center" onPress={() => { setChat([]); _init(); }}>
+          <TouchableOpacity
+            className="p-2 bg-primary rounded-full items-center justify-center"
+            onPress={() => {
+              setChat([]);
+              _init();
+            }}
+          >
             <Ionicons name="refresh" size={24} color="white" />
           </TouchableOpacity>
         </View>
       </View>
 
       <View className="flex-12  p-4">
-        <FlatList className="h-full w-full bg-background"
-          showsVerticalScrollIndicator={false} data={chat} keyExtractor={(item, index) => index.toString()} renderItem={({ item }) => (
+        <FlatList
+          className="h-full w-full bg-background"
+          showsVerticalScrollIndicator={false}
+          data={chat}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) =>
             item.role === "user" ? (
               <View className="flex-row items-center justify-end mb-4">
                 <View className="p-2 px-4 bg-primary rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl">
-                  <Text className="text-white font-pmedium">{item.content}</Text>
+                  <Text className="text-white font-pmedium">
+                    {item.content}
+                  </Text>
                 </View>
               </View>
             ) : (
-              <View
-                className="flex-col items-start justify-start mb-4"
-              >
+              <View className="flex-col items-start justify-start mb-4">
                 <View className="flex-row items-center justify-start relative">
                   <View>
-                    <Image source={require("../../../assets/ChatGPT-Logo.png")} className="w-8 h-8 rounded-full absolute " />
+                    <Image
+                      source={require("../../../assets/ChatGPT-Logo.png")}
+                      className="w-8 h-8 rounded-full absolute "
+                    />
                   </View>
                   <View className="p-2 px-4 bg-[#2f2f44] rounded-tl-3xl rounded-tr-3xl rounded-br-3xl ml-10">
-                    <Text className="text-white font-pmedium">{item.content}</Text>
+                    {item.content === "Loading..." ? (
+                      <Text>Loading...</Text>
+                    ) : (
+                      <Text className="text-white font-pmedium">
+                        {item.content}
+                      </Text>
+                    )}
                   </View>
                 </View>
               </View>
-
             )
-          )} />
+          }
+        />
 
         <View className="w-full h-16 px-2 flex-row items-center justify-around  ">
-          <View className="flex-6 p-2 px-4 font-pmedium  border border-t border-white rounded-full">
+          <View
+            className={`flex-6 p-2 px-4 font-pmedium  border border-t ${
+              loading ? "border-whitesmoke" : "border-white"
+            } rounded-full`}
+          >
             <TextInput
               className="text-white"
               placeholder={`Merhaba ${id}`}
               value={chatMeet}
               onChangeText={(text) => setChatMeet(text)}
+              editable={!loading}
             />
           </View>
           <View className="flex-1 items-center justify-center ml-4">
-            <TouchableOpacity className="p-2 bg-primary rounded-full items-center justify-center" onPress={() => { sendMessage(chatMeet); setChatMeet(""); }}>
-              <Ionicons name="send-outline" size={24} color="white" />
+            <TouchableOpacity
+              className="p-2 bg-primary rounded-full items-center justify-center"
+              onPress={() => {
+                sendMessage(chatMeet);
+                setChatMeet("");
+              }}
+            >
+              {!loading ? (
+                <Ionicons name="send-outline" size={24} color="white" />
+              ) : (
+                <ActivityIndicator size="small" color="white" />
+              )}
             </TouchableOpacity>
           </View>
         </View>
